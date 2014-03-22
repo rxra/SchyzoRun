5ecb8c98dd92b771aaccd360e2a591f325c9bf8a
Nicolas Hognon <nicolas.hognon@gmail.com>
2014-03-22 01:10:20 +0100

    first POC of spliting screens.

---
+++ UnityProject/Assets/Code/Screen.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screen : MonoBehaviour {
+
+	public int level = 0;
+	public Color color = Color.blue;
+
+	public void Split(bool horizontal)
+	{
+		Debug.Log ("++ Split: " + gameObject.name);
+		Screen s1 = GameObject.Instantiate(this) as Screen;
+		Screen s2 = GameObject.Instantiate(this) as Screen;
+
+		s1.level = level+1;
+		s2.level = level+1;
+		s1.color = new Color(Random.Range(0f,1f),Random.Range(0f,1f), Random.Range(0f,1f));
+		s2.color = new Color(Random.Range(0f,1f),Random.Range(0f,1f), Random.Range(0f,1f));
+
+		if (horizontal) {
+			s1.gameObject.name = "Screen_"+s1.level+"_up";
+			s1.transform.localScale = new Vector3(
+				transform.localScale.x,
+				transform.localScale.y/2f,
+				transform.localScale.z
+			);
+			s1.transform.position = new Vector3(
+				transform.position.x,
+				transform.position.y + s1.transform.localScale.y/2f,
+				transform.position.z
+			);
+
+
+			s2.gameObject.name = "Screen_"+s1.level+"_down";
+			s2.transform.localScale = new Vector3(
+				transform.localScale.x,
+				transform.localScale.y/2f,
+				transform.localScale.z
+				);
+			s2.transform.position = new Vector3(
+				transform.position.x,
+				transform.position.y - s2.transform.localScale.y/2f,
+				transform.position.z
+				);
+		} else {
+			s1.gameObject.name = "Screen_"+s1.level+"_right";
+			s1.transform.localScale = new Vector3(
+				transform.localScale.x/2f,
+				transform.localScale.y,
+				transform.localScale.z
+				);
+			s1.transform.position = new Vector3(
+				transform.position.x + s1.transform.localScale.x/2f,
+				transform.position.y,
+				transform.position.z
+				);
+
+			s2.gameObject.name = "Screen_"+s1.level+"_left";
+			s2.transform.localScale = new Vector3(
+				transform.localScale.x/2f,
+				transform.localScale.y,
+				transform.localScale.z
+				);
+			s2.transform.position = new Vector3(
+				transform.position.x - s2.transform.localScale.x/2f,
+				transform.position.y,
+				transform.position.z
+				);
+		}
+
+		ScreenManager.instance.AddScreen(s1);
+		ScreenManager.instance.AddScreen(s2);
+		ScreenManager.instance.RemoveScreen(this,true);
+
+		Debug.Log ("-- Split");
+	}
+
+	void Start () {
+		renderer.material.color = color;
+	}
+	
+}
---
+++ UnityProject/Assets/Code/ScreenManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScreenManager : MonoBehaviour {
+
+	public Screen screenPrefab;
+	public LayerMask screenLayer;
+
+	public static ScreenManager instance
+	{
+		get
+		{
+			return s_Instance;
+		}
+	}
+
+	public void AddScreen(Screen s)
+	{
+		_screens.Add (s);
+		Debug.Log ("screens: " + _screens.Count);
+	}
+
+	public void RemoveScreen(Screen s, bool destroy)
+	{
+		_screens.Remove(s);
+		if (destroy) {
+			GameObject.Destroy(s.gameObject);
+		}
+		Debug.Log ("screens: " + _screens.Count);
+	}
+
+	static private ScreenManager s_Instance = null;
+
+	void Awake () {
+		if (s_Instance) {
+			Debug.LogError ("Error: an instance of ScreenManager already exist");
+			return;
+		}
+		s_Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		Screen screen = GameObject.Instantiate(screenPrefab,Vector3.zero,Quaternion.identity) as Screen;
+		screen.level = 0;
+		screen.gameObject.name = "Screen_" + screen.level;
+		AddScreen(screen);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonUp (0)) {
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out _mouseHit, screenLayer)) {
+				Screen s = _mouseHit.collider.gameObject.GetComponent<Screen>();
+				if (s==null) {
+					Debug.LogWarning("Cannot find Screen compoment on hitted object");
+				} else {
+					s.Split(_horizontal);
+					_horizontal = !_horizontal;
+				}
+			}
+		}	
+
+	}
+
+	private List<Screen> _screens = new List<Screen>();
+	private RaycastHit _mouseHit = new RaycastHit();
+	private bool _horizontal = true;
+
+}
