90659884f242cfa0baf05d811e7116c9b36a77e8
Nicolas Hognon <nicolas.hognon@gmail.com>
2014-03-23 21:35:36 +0100

    timer sur le gameover

--- UnityProject/Assets/Code/GameOver.cs
+++ UnityProject/Assets/Code/GameOver.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class GameOver : MonoBehaviour {
 
+	public float timerBeforeKey = 2;
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.anyKey) {
+		if (Time.timeSinceLevelLoad>timerBeforeKey && Input.anyKey) {
 			Application.LoadLevel(1);
 		}
 	}
+	
 }
